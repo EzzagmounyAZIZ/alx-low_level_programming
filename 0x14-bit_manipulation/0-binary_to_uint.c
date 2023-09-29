6d8667d7978097eee1cabf95ac9bd94f3f62be92
EzzagmounyAZIZ <azizezzagmouny@gmail.com>
2023-09-29 07:43:43 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,25 +9,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-
 	if (b == NULL)
 		return (0);
-
-	int i = 0;
-
-	while (b[i] != '\0')
+	while (*b != '\0')
 	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			result = (result << 1) + (b[i] - '0');
-			i++;
-		}
-		else
-		{
+		if (*b != '0' && *b != '1')
 			return (0);
-		}
+		result = (result << 1) + (*b - '0');
+		b++;
 	}
-
 	return (result);
-
 }
