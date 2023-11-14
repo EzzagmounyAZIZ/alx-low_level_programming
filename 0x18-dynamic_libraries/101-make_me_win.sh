6353ddf2e7f82a6f59c095000f19de2c3c3a1147
EzzagmounyAZIZ <azizezzagmouny@gmail.com>
2023-11-13 16:26:51 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/EzzagmounyAZIZ/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD8PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/EzzagmounyAZIZ/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD8PRELOAD="$PWD/../libgiga.so"
