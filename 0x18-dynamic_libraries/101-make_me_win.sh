1972dbfc3dd050dcdae6d11b3ea777b0e8d8aa6d
EzzagmounyAZIZ <azizezzagmouny@gmail.com>
2023-11-14 01:50:24 -0800

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/EzzagmounyAZIZ/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD8PRELOAD="$PWD/../libgiga.so"
+wget -P /tmp/ https://github.com/EzzagmounyAZIZ/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD =/tmp/libgiga.so
